2c4fad2a93784c45d41f70b4e5f6b709b1ce9a04
Gilbert Rios <gilbertrios@hotmail.com>
2025-11-07 07:30:27 -0600

    add prod environment

---
+++ environments/dev/variables.tf
@@ -0,0 +1,15 @@
+variable "environment" {
+  description = "Environment name"
+  type        = string
+}
+
+variable "resource_group_name" {
+  description = "Name of the resource group"
+  type        = string
+}
+
+variable "location" {
+  description = "Azure region"
+  type        = string
+  default     = "East US"
+}
